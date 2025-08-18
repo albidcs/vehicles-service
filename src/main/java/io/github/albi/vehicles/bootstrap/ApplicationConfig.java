292b9271c50009a0424bdff1387612f7a0bc7c9b
Albi Dushi <15907450+albidcs@users.noreply.github.com>
2025-08-18 12:35:37 +0100

    build(bootstrap): enable component scan + JPA repositories/entities
    - Annotated VehiclesApplication with @ComponentScan("io.github.albi.vehicles")
    - Enabled JPA repositories via @EnableJpaRepositories(basePackageClasses = VehicleJpaRepository.class)
    - Registered entity scanning via @EntityScan(basePackageClasses = VehicleEntity.class)
    - Added ApplicationConfig to expose VehicleService bean
    
    Outcome: controllers/adapters are detected and VehicleJpaRepository is
    wired at startup.

---
+++ src/main/java/io/github/albi/vehicles/bootstrap/ApplicationConfig.java
@@ -0,0 +1,16 @@
+package io.github.albi.vehicles.bootstrap;
+
+import io.github.albi.vehicles.application.vehicle.VehicleService;
+import io.github.albi.vehicles.domain.vehicle.VehicleRepository;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+class ApplicationConfig {
+    @Bean
+    VehicleService vehicleService(VehicleRepository repo) {
+        return new VehicleService(repo);
+    }
+
+
+}
--- src/main/java/io/github/albi/vehicles/bootstrap/VehiclesApplication.java
+++ src/main/java/io/github/albi/vehicles/bootstrap/VehiclesApplication.java
@@ -1,9 +1,18 @@
 package io.github.albi.vehicles.bootstrap;
 
+import io.github.albi.vehicles.adapters.persistence.vehicle.VehicleEntity;
+import io.github.albi.vehicles.adapters.persistence.vehicle.VehicleJpaRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
 
 @SpringBootApplication
+@ComponentScan(basePackages = "io.github.albi.vehicles")
+@EnableJpaRepositories(basePackageClasses = VehicleJpaRepository.class)
+@EntityScan(basePackageClasses = VehicleEntity.class)
 public class VehiclesApplication {
 
     public static void main(String[] args) {
